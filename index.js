9076c343912a263c45acdde39e1bf0ceb635589e
IkokoOff <159687221+IkokoOff@users.noreply.github.com>
2024-03-21 16:34:31 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+
+// Replace 'YOUR_TOKEN' with your bot's token
+const token = process.env.TOKEN;
+
+async function updateAvatar() {
+    try {
+        const newAvatar = fs.readFileSync('avatar.gif'); // Path to the new avatar image file
+        const response = await fetch('https://discord.com/api/v9/users/@me', {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bot ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                avatar: `data:image/gif;base64,${newAvatar.toString('base64')}`
+            })
+        });
+
+        if (response.ok) {
+            console.log('Avatar updated successfully!');
+        } else {
+            console.error('Failed to update avatar:', response.statusText);
+            const responseBody = await response.text();
+            console.error('Response body:', responseBody);
+        }
+    } catch (error) {
+        console.error('Error updating avatar:', error);
+    }
+}
+
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
+});
+
+
+const statusMessages = ["Manage 𝗙𝗢𝗥𝗧𝗡𝗜𝗧𝗘 𝗚𝗟𝗜𝗧𝗖𝗛"];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(token);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+
+  // Call updateAvatar() here if you want to update the avatar when the bot is ready
+  updateAvatar();
+});
+
+login();
